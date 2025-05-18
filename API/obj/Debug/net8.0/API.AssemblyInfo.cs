7d59d0e139f94a8d8d6edbc56b3f73285daa95da
trym-s <velihandogrul@icloud.com>
2025-05-18 21:04:55 +0300

    Shared Kernel

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+511f6f53a0c8c32e2230113b16015df589bd60c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2a8a16bc083869e1fb1fca5498bd2c1cbd15662")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
